3a8d28def48aef1eee160d6b08dd7ed6b6220916
tum9969 <135408624+tum9969@users.noreply.github.com>
2025-01-19 12:52:06 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+char* reverse( char str1[]) ;
+
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
+    char text[ 50 ] ;
+    char *output ;
+    output = reverse( text ) ;
+    printf( "MAIN : %s\n", output ) ;
 }//end function
+
+char* reverse( char str1[] ) {
+    char *output ;
+    int N = 999, i ;
+    printf( "Enter str1 : " ) ;
+    gets( str1 ) ;
+    for( i = 0 ; i < N ; i++ ) if( str1[i] == '\0' ) break ;
+    N = i ;
+    output = new char[ N + 1 ] ;
+    for( i = 0 ; i < N ; i++ ) output[ i ] = str1[ N - i - 1 ] ;
+    output[i] = '\0' ;
+    printf( "FUNC : %s\n", output ) ;
+    return output ;
+}
