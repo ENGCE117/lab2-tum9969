ba3bc234f8c42f49287f04d0187993898faf8c5a
tum9969 <135408624+tum9969@users.noreply.github.com>
2025-01-21 16:29:05 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,26 +1,7 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-char* reverse( char str1[]) ;
 
 int main() {
-    char text[ 50 ] ;
-    char *output ;
-    output = reverse( text ) ;
-    printf( "MAIN : %s\n", output ) ;
+    printf( ". eM dna uoY\n" ) ;
+    printf( ". eM dna uoY\n" ) ;
 }//end function
 
-char* reverse( char str1[] ) {
-    char *output ;
-    int N = 999, i ;
-    printf( "Enter str1 : " ) ;
-    gets( str1 ) ;
-    for( i = 0 ; i < N ; i++ ) if( str1[i] == '\0' ) break ;
-    N = i ;
-    output = new char[ N + 1 ] ;
-    for( i = 0 ; i < N ; i++ ) output[ i ] = str1[ N - i - 1 ] ;
-    output[i] = '\0' ;
-    printf( "FUNC : %s\n", output ) ;
-    return output ;
-}
