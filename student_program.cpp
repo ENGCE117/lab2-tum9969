18dd2c1bd52be966c2666b8186fc8e7bd6087257
tum9969 <135408624+tum9969@users.noreply.github.com>
2025-01-22 12:50:16 +0700

    ส่งแล็บไว้ที่ไฟล์ student_program.cpp
    เอา Fix Code มาใส่แล้ว และพึ่ง Implement โค้ดเอาไว้ด้านล่าง เพื่อทดสอบว่าจะส่งผ่านรึปล่าว

--- student_program.cpp
+++ student_program.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+char* reverse( char str1[]) ;
+
 int main() {
-    printf( ". eM dna uoY\n" ) ;
-    printf( ". eM dna uoY\n" ) ;
+    char text[ 50 ] ;
+    char *output ;
+    output = reverse( text ) ;
+    printf( "MAIN : %s\n", output ) ;
 }//end function
 
+char* reverse( char str1[]) {
+    char *a = new char[ 20 ] ;
+    return *a ;
+}
